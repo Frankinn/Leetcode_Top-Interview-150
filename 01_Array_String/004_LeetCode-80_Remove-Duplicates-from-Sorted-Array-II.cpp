ddcac72dd33ae705e0bcb7f9f9a691e9cf10ee5b
Frankinn <55120016+Frankinn@users.noreply.github.com>
2026-05-04 16:21:04 +0800

    Optimize: General Two-Pointer Template (K-step Lookback)

--- 01_Array_String/004_LeetCode-80_Remove-Duplicates-from-Sorted-Array-II.cpp
+++ 01_Array_String/004_LeetCode-80_Remove-Duplicates-from-Sorted-Array-II.cpp
@@ -3,11 +3,23 @@
  * Problem: 80. Remove Duplicates from Sorted Array II (Medium)
  * ---------------------------------------------------------
  * Progress:
- * - Version 1: Original
+ * - Version 1: Manual State Tracking
+ * - Version 2: General Two-Pointer Template (K-step Lookback)
  */
 class Solution {
+private:
+    static const int MAX_TIMES = 2;
 public:
     int removeDuplicates(vector<int>& nums) {
+        if(nums.size() <= MAX_TIMES) return nums.size();
+        int slow = MAX_TIMES;
+        for(int fast = MAX_TIMES; fast < nums.size(); ++fast){
+            if(nums[fast] != nums[slow-2]){
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+        /*
         if(!nums.size()) return 0;
         if(nums.size() == 1) return 1;
         if(nums.size() == 2) return 2;
@@ -24,5 +36,6 @@ public:
             }
         }
         return slow;
+        */
     }
 };
