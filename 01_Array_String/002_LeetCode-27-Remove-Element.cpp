959da9e143e23b53f9d13dacf23ef5b406e9892f
Frankinn <55120016+Frankinn@users.noreply.github.com>
2026-05-04 15:09:22 +0800

    Optimize: simplify code

--- 01_Array_String/002_LeetCode-27-Remove-Element.cpp
+++ 01_Array_String/002_LeetCode-27-Remove-Element.cpp
@@ -4,14 +4,25 @@
  * ---------------------------------------------------------
  * Progress:
  * - Version 1: Two Pointers - Head-to-Tail Swap
+ * - Version 2: Simplify code
  */
- class Solution {
+ //若val數量很多，可以使用簡潔的快慢指標
+class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        for(int i = 0; i < n;){
+            if(nums[i] == val)
+                nums[i] = nums[--n];
+            else
+                ++i;
+        }
+        return n;
+        /* //ver1
         int k = 0;
-        /*if(nums.size() == 1){
-            return (nums[0] == val ? 0 : 1);
-        }*/
+        //if(nums.size() == 1){
+        //    return (nums[0] == val ? 0 : 1);
+        //}
         for(int i = 0, j = nums.size(); i < j; ++i){
             if(nums[i] == val){
                 ++k;
@@ -28,5 +39,6 @@ public:
             }
         }
         return nums.size() - k;
+        */
     }
 };
--- 01_Array_String/README.md
+++ 01_Array_String/README.md
@@ -1,2 +1,2 @@
-| 001 | Easy | 88 | [Merge Sorted Array](./001_LeetCode-88_Merge-Sorted-Array.cpp)
-| 002 | Easy | 27 | [Remove Element](./002_LeetCode-27_Remove-Element.cpp)
+| 001 | Easy | 88 | [Merge Sorted Array](./001_LeetCode-88_Merge-Sorted-Array.cpp)  
+| 002 | Easy | 27 | [Remove Element](./002_LeetCode-27_Remove-Element.cpp)  
